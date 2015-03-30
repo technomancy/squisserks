081fdd7bcc2942348413338f96315f8d83a2d7e7
Phil Hagelberg <phil@hagelb.org>
2015-03-30 16:29:30 +0700

    Fix acceleration in fly mode.

--- fly.rkt
+++ fly.rkt
@@ -9,34 +9,52 @@
                        (beside one-scene one-scene))])
     (crop (modulo x width) (modulo y height) width height scene)))
 
-(define ship (overlay (isosceles-triangle 60 30 "solid" "aquamarine")
-                      (circle 40 "outline" "aquamarine")))
+(define ship (overlay (isosceles-triangle 30 15 "solid" "aquamarine")
+                      (circle 20 "outline" "aquamarine")))
 
 (define (draw-fly now)
   (match (now 'mode-data)
-    [(hash-table ('x x) ('y y) ('dir dir) _)
+    [(hash-table ('x x) ('y y) ('dir dir) _ ...)
      (underlay (scene-offset x y)
                (rotate dir ship))]))
 
+
+
 (define (tick-fly now)
   (match (now 'mode-data)
-    [(hash-table ('x x) ('y y) ('dir dir) ('speed speed))
-     (let ([dx 5] [dy 5])
-       (now 'mode-data (hash-set* (now 'mode-data)
-                                  'x (+ x dx) 'y (+ y dy))))]))
+    [(hash-table ('x x) ('y y) ('dy dy) ('dx dx) _ ...)
+     (now 'mode-data (hash-set* (now 'mode-data)
+                                'x (round (+ x dx))
+                                'y (round (+ y dy))))]))
+
+
+
+(define engine 1)
+(define max-speed 25)
+
+(define (accel mode-data)
+  (match mode-data
+    [(hash-table ('dy dy) ('dx dx) ('dir dir) _ ...)
+     (let ([ddx (* engine (sin (degrees->radians dir)))]
+           [ddy (* engine (cos (degrees->radians dir)))])
+       (if (< (+ dx dy) max-speed)
+           (hash-set* mode-data
+                      'dx (round (- dx ddx))
+                      'dy (round (- dy ddy)))
+           mode-data))]))
 
 (define (keys-fly now pressed)
   (let* ([m (now 'mode-data)]
-         [m (cond [(key=? pressed "up") (hash-update m 'speed add1)]
-                  [(key=? pressed "down") (if (positive? (hash-ref m 'speed))
-                                              (hash-update m 'speed sub1)
-                                              m)]
+         [m (cond [(key=? pressed "up") (accel m)]
                   [(key=? pressed "left") (hash-update m 'dir (curryr + 10))]
                   [(key=? pressed "right") (hash-update m 'dir (curryr - 10))]
-                  [else now])])
+                  [else m])])
     (now 'mode-data m)))
+
+
 
 (define (fly now)
   (let ([now (now 'mode 'fly)])
     (now 'mode-data #hash((x . 0) (y . 0)
-                          (dir . 0) (speed . 1)))))
+                          (dx . 0) (dy . 0)
+                          (dir . 0)))))
